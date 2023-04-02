44f863eb254063e5bfc6bdae3a254d757a24e270
lee-ji-an <leeja0407@naver.com>
2023-04-02 13:09:22 +0900

    Set security group

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -24,6 +25,16 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform-example"
   }
+}
 
+# 8080 포트에 대한 트래픽 허용
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
 
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 모든 IP 주소 대역 포함
+  }
 }
