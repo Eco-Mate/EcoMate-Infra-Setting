61e4a9ac923c4780ad21875029e76669c1474964
lee-ji-an <leeja0407@naver.com>
2023-04-02 13:24:24 +0900

    Change server port to variable

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+variable "server_port" {
+  description = "The port the serve will use for HTTP requests"
+  default = 8080
+}
+
 provider "aws" {
   region = "us-east-1"  # 북부 버지니아
 }
@@ -14,7 +19,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -32,8 +37,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # 모든 IP 주소 대역 포함
   }
