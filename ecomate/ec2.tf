9d79235934ba537018e9a1982cfe832c84a6c0f0
lee-ji-an <leeja0407@naver.com>
2023-07-10 22:34:37 +0900

    Add security group to ec2 instance(ecomate_project)

--- ecomate/ec2.tf
+++ ecomate/ec2.tf
@@ -1,4 +1,49 @@
 resource "aws_instance" "ecomate_instance" {
   ami = "ami-0c9c942bd7bf113a2"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.ecomate_sg.id ]
+  tags = {
+    Name = "ecomate"
+  }
+}
+
+resource "aws_security_group" "ecomate_sg" {
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    description     = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "ecomate_instance_sg"
+  }
+}
+
+# ssh 연결 허용
+resource "aws_security_group_rule" "ecomate_sg_rule_ssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecomate_sg.id
+}
+
+# https 연결 허용
+resource "aws_security_group_rule" "ecomate_sg_rule_https" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecomate_sg.id
 }
